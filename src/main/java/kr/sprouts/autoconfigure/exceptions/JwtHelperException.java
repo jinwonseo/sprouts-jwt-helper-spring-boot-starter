1697f815aac67059735155ef8d057f1eaf27572e
Jinwon Seo <jwseo@live.co.kr>
2022-12-14 17:15:30 +0900

    Initialize project. Closed #1

---
+++ src/main/java/kr/sprouts/autoconfigure/exceptions/JwtHelperException.java
@@ -0,0 +1,7 @@
+package kr.sprouts.autoconfigure.exceptions;
+
+public class JwtHelperException extends RuntimeException {
+    public JwtHelperException(String message) {
+        super(message);
+    }
+}
