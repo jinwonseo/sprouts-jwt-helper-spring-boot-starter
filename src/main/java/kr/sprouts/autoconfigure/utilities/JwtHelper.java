7efdb7faafcf06a29223eb20ac56e74ce3f19310
Jinwon Seo <jwseo@live.co.kr>
2022-12-15 13:37:32 +0900

    Initialize log 변경 & Helper Bean to Static class. Closed #4

--- src/main/java/kr/sprouts/autoconfigure/utilities/JwtHelper.java
+++ src/main/java/kr/sprouts/autoconfigure/utilities/JwtHelper.java
@@ -1,22 +1,14 @@
-package kr.sprouts.autoconfigure.components;
+package kr.sprouts.autoconfigure.utilities;
 
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.security.Key;
 import java.security.KeyPair;
 import java.util.Base64;
 
 public class JwtHelper {
-    private final Logger logger = LoggerFactory.getLogger(JwtHelper.class);
-
-    public JwtHelper() {
-        this.logger.info(String.format("Initialized %s", JwtHelper.class.getName()));
-    }
-
     public static Key secretKeyFor(SignatureAlgorithm signatureAlgorithm) {
         return Keys.secretKeyFor(signatureAlgorithm);
     }
@@ -25,12 +17,12 @@ public class JwtHelper {
         return Keys.keyPairFor(signatureAlgorithm);
     }
 
-    public static Base64UrlEncodedKey base64urlEncodedSecretKeyFor(SignatureAlgorithm signatureAlgorithm) {
-        return Base64UrlEncodedKey.of(Keys.secretKeyFor(signatureAlgorithm));
+    public static Base64UrlEncodedSecret base64urlEncodedSecretKeyFor(SignatureAlgorithm signatureAlgorithm) {
+        return Base64UrlEncodedSecret.of(Keys.secretKeyFor(signatureAlgorithm));
     }
 
-    public static Base64UrlEncodedKeyPair base64UrlEncodedKeyPairFor(SignatureAlgorithm signatureAlgorithm) {
-        return Base64UrlEncodedKeyPair.of(Keys.keyPairFor(signatureAlgorithm));
+    public static Base64UrlEncodedSecretPair base64UrlEncodedKeyPairFor(SignatureAlgorithm signatureAlgorithm) {
+        return Base64UrlEncodedSecretPair.of(Keys.keyPairFor(signatureAlgorithm));
     }
 
     public static Key convertToKey(String base64UrlEncodedSecret) {
